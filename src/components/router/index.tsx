199423f201883a8a7ae1ffdf7818242864f538df
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-05 17:34:52 +0300

    paths added

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -2,6 +2,10 @@ import Home from "../../pages/Home";
 import Users from "../../pages/Users";
 import User from "../../pages/User";
 import NotFoundPage from "../../pages/NotFoundPage";
+import Posts from "../../pages/Posts";
+import Post from "../../pages/Post";
+import Albums from "../../pages/Albums";
+import Album from "../../pages/Album";
 
 
 
@@ -9,6 +13,10 @@ export const publicRoutes=[
     {path:'/', element:<Home/>},
     {path:'/users', element:<Users/>},
     {path:'/users/id:', element:<User/>},
+    {path:'/posts',element: <Posts/>},
+    {path:'/posts/:id',element: <Post/>},
+    {path:'/albums',element: <Albums/>},
+    {path:'/albums/:id',element: <Album/>},
     {path: '*',element: <NotFoundPage/>}
 
 ]
