4764978c9c711243d3e0be39ff7a6f735e807afd
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-05 18:43:29 +0300

    refactoring users

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,9 +1,43 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
+import {useParams} from "react-router-dom";
+import {useTypedSelector} from "../hooks/useTypedSelector";
+import {useActions} from "../hooks/useActions";
 
-const User:FC = () => {
+type UserItemPageParams = {
+    id: string
+}
+const User: FC = () => {
+    const params = useParams<UserItemPageParams>()
+    const {users,loading,error}=useTypedSelector(state => state.user)
+    const {fetchUser}=useActions()
+    useEffect(()=>{
+
+        switch (typeof params.id){
+            case 'string':
+                fetchUser(parseInt(params.id))
+                break
+            default:
+                fetchUser(-1)
+                break
+        }
+    },[])
+    if (loading){
+        return (
+            <h1>Идёт загрузка...</h1>
+        )
+    }
+    if(error){
+        return (
+            <h1>Произошла ошибка</h1>
+        )
+    }
+
     return (
+
         <div>
-            Один пользователь
+            {users.map(user =>
+                <div key={user.id}>{user.id}---{user.name}</div>
+            )}
         </div>
     );
 };
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,9 +1,18 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
+import {useTypedSelector} from "../hooks/useTypedSelector";
+import {useActions} from "../hooks/useActions";
 
-const Users:FC = () => {
+const Users: FC = () => {
+    const {users, error, loading} = useTypedSelector(state => state.user)
+    const {fetchUsers} = useActions();
+    useEffect(() => {
+        fetchUsers()
+    }, [])
     return (
         <div>
-            Много пользователей
+            {users.map(user =>
+                <div key={user.id}>{user.name}</div>
+            )}
         </div>
     );
 };
