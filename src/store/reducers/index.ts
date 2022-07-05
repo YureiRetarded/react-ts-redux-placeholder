340598b215dad07807fec33392ccfe099edf9e8f
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-05 18:05:05 +0300

    redecers refactoring

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
 import {userReducer} from "./userReduser";
+import {postReducer} from "./postReducer";
 
 export const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    post:postReducer,
 })
 export type RootState = ReturnType<typeof rootReducer>
