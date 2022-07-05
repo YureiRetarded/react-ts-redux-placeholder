8f344f1266360a2bc871d6b7d2f56242fc9eb880
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-05 14:51:18 +0300

    text formating

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,50 +1,57 @@
-export interface IUser{
-    id:number;
-    name:string;
-    username:string;
-    email:string;
-    address:IAddress;
-    phone:string;
-    website:string;
-    company:ICompany;
+export interface IUser {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: IAddress;
+    phone: string;
+    website: string;
+    company: ICompany;
 }
-export interface IAddress{
-    street:string;
-    suite:string;
-    city:string;
-    zipcode:string;
-    geo:IGeo;
+
+export interface IAddress {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: IGeo;
 }
-export interface IGeo{
-    lat:string;
-    lng:string;
+
+export interface IGeo {
+    lat: string;
+    lng: string;
 }
-export interface ICompany{
-    name:string;
-    catchPhrase:string;
-    bs:string;
+
+export interface ICompany {
+    name: string;
+    catchPhrase: string;
+    bs: string;
 }
-export interface ITodo{
-    id:number;
-    userId:number;
-    title:string;
-    completed:boolean;
+
+export interface ITodo {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean;
 }
-export interface IPhotos{
-    id:number;
-    albumId:number;
-    title:string;
-    url:string;
-    thumbnailUrl:string;
+
+export interface IPhotos {
+    id: number;
+    albumId: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
 }
-export interface IAlbum{
-    id:number;
-    userId:number;
-    title:string;
+
+export interface IAlbum {
+    id: number;
+    userId: number;
+    title: string;
 }
-export interface IPost{
-    id:number;
-    userId:number;
-    title:string;
-    body:string;
+
+export interface IPost {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
 }
