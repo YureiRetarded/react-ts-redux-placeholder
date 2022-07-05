2cdfa6c70a285096975e7e8fdcf614aee48ff60b
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-05 23:27:27 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UserList from "./components/UserList";
 import {BrowserRouter as Router} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
-import NavbarReact from "./components/NavbarReact/NavbarReact";
+import NavbarReact from "./components/UI/NavbarReact/NavbarReact";
 
 function App() {
     return (
