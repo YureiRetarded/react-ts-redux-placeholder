88ead55d4f2938617de7c080bce226aa7a7c0730
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-05 16:58:25 +0300

    Test pages created

---
+++ src/pages/User.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const User:FC = () => {
+    return (
+        <div>
+            Один пользователь
+        </div>
+    );
+};
+
+export default User;
---
+++ src/pages/Users.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+
+const Users:FC = () => {
+    return (
+        <div>
+            Много пользователей
+        </div>
+    );
+};
+
+export default Users;
