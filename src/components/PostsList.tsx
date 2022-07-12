ed45e912e1a62de1b0dbb621225169d22bcdcbb3
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-12 21:56:18 +0300

    more works

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,30 +1,48 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useRef} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {useActions} from "../hooks/useActions";
 import PostItem from "./PostItem";
-import {Button, Stack} from "react-bootstrap";
+import {Stack} from "react-bootstrap";
+
 
 const PostsList: FC = () => {
-    const {posts, loading, error,totalCount,totalPage,page,limit} = useTypedSelector(state => state.post)
-    const {fetchPosts,setPostPage}=useActions()
-    console.log(totalCount)
-    console.log(totalPage)
-    console.log(page)
-    useEffect(()=>{
-        fetchPosts(limit,page)
+    const {posts, loading, error, totalCount, totalPage, page, limit} = useTypedSelector(state => state.post)
+    const {fetchPosts, setPostPage} = useActions()
+    const observer=useRef<IntersectionObserver>();
+    const targetRef=useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        fetchPosts(limit, page)
+    }, [page])
 
-    },[page])
-    const test=()=>{
-        if(page!=totalPage){
-            setPostPage(page+1)
+    useEffect(() => {
+        let callback = function(entries:any, observer:any) {
+            if(entries[0].isIntersecting){
+
+                console.log('Текущяя страница '+page)
+                console.log('Ожидаемая страница '+(page+1))
+                console.log('Всего страниц '+totalPage)
+                setPostPage(page + 1)
+            }
+        };
+        observer.current = new IntersectionObserver(callback);
+        observer.current?.observe(targetRef.current as Element)
+    }, [loading])
+
+    const test = () => {
+        if (page != totalPage) {
+            setPostPage(page + 1)
         }
+        console.log('Текущяя страница '+page)
+        console.log('Ожидаемая страница '+(page+1))
+        console.log('Всего страниц '+totalPage)
     }
     return (
-        <Stack gap={5} >
+        <Stack gap={5}>
             <button onClick={test}>+</button>
-            {posts.map(post=>
-                <PostItem key={post.id} post={post} />
+            {posts.map(post =>
+                <PostItem key={post.id} post={post}/>
             )}
+            <div ref={targetRef} style={{height:25, backgroundColor:"black"}}/>
         </Stack>
     );
 };
