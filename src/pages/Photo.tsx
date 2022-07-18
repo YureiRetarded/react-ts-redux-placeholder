c121198b4f790f5c4426611c8899d545a1ced98b
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-07-18 19:00:26 +0300

    -

--- src/pages/Photo.tsx
+++ src/pages/Photo.tsx
@@ -1,18 +1,22 @@
 import React, {FC, useEffect} from 'react';
 import {useTypedSelector} from "../hooks/useTypedSelector";
 import {useActions} from "../hooks/useActions";
-import {useParams} from "react-router-dom";
-import {Card, Container} from "react-bootstrap";
+import {useNavigate, useParams} from "react-router-dom";
+import {Button, Card, Container} from "react-bootstrap";
 
 const Photo: FC = () => {
-    const {photo,loading,error}=useTypedSelector(state => state.photo)
-    const {fetchPhoto}=useActions()
-    const {photoId}=useParams()
-    useEffect(()=>{
-        fetchPhoto(parseInt(photoId||'1'))
-    },[])
+    const navigate = useNavigate();
+    const {userId, albumId} = useParams()
+    const {photo, loading, error} = useTypedSelector(state => state.photo)
+    const {fetchPhoto} = useActions()
+    const {photoId} = useParams()
+    useEffect(() => {
+        fetchPhoto(parseInt(photoId || '1'))
+    }, [])
     return (
         <Container className='p-0 pt-3'>
+            <Button onClick={() => navigate('/users/' + userId + '/album/' + albumId)}
+                    className='w-100 mb-3'>Back</Button>
             <Card>
                 <Card.Img variant="top" src={photo.url} alt="Card image"/>
                 <Card.Title>
